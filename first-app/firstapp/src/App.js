7cb53c907786586c9cdcf16e0462c9adfd7177d0
Ben Harter-Murphy <benhartermurphy@gmail.com>
2022-05-14 17:26:31 -0400

    finished section1

--- first-app/firstapp/src/App.js
+++ first-app/firstapp/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Hello from './components/Hello';
+import Hello2 from './components/Hello2'
 
 function App() {
   return (
     <div className="App">
       <Hello/>
+      <Hello2/>
+
     </div>
   );
 }
--- first-app/firstapp/src/components/Hello.js
+++ first-app/firstapp/src/components/Hello.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 
 class Hello extends Component{
+    // class component 
     render(){
-        return <div>
+        return (
+        <div>
+            <h1>Hello there!</h1>
+            <h1>Hello there!</h1>
             <h1>Hello there!</h1>
         </div>
-    }
+        )
+}
 }
 
 export default Hello
